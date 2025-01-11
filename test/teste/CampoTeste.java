d81fc2146c88f3dd1d91237faecbd83d8e2fcfc2
BrendoXR <114269551+BrendoXR@users.noreply.github.com>
2025-01-10 21:02:43 -0400

    Lançando projeto

---
+++ test/teste/CampoTeste.java
@@ -0,0 +1,47 @@
+package teste;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import excecao.ExplosaoException;
+import modelo.Campo;
+
+class CampoTeste {
+
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3,3);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancia1() {
+	Campo vizinho = new Campo(3,2);	
+	boolean resultado = campo.adicionarVizinho(vizinho);
+	assertTrue(resultado);
+	}
+	
+	@Test
+	void testeAbrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoNaoMarcado() {
+		campo.minar();
+		
+		assertThrows(ExplosaoException.class, () -> {
+			campo.abrir();
+		});
+	}
+	
+}
+
+
