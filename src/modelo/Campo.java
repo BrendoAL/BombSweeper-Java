71babe0f7bac0209310f20da360ffe6352a63799
BrendoXR <114269551+BrendoXR@users.noreply.github.com>
2025-01-28 23:06:46 -0400

    Adição do Swing para a criação da interface gráfica

--- src/modelo/Campo.java
+++ src/modelo/Campo.java
@@ -3,8 +3,6 @@ package modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-import excecao.ExplosaoException;
-
 public class Campo {
 
 	private final int linha;
@@ -15,28 +13,38 @@ public class Campo {
 	private boolean marcado = false;
 	
 	private List<Campo> vizinhos = new ArrayList<>();
+	private List<CampoObservador> observadores = new ArrayList<>();
 	
-	public Campo(int linha, int coluna){
+	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
 	
-	public boolean adicionarVizinho(Campo vizinho) {
+	public void registrarObservador(CampoObservador observador) {
+		observadores.add(observador);
+	}
+	
+	private void notificarObservadores(CampoEvento evento) {
+		observadores.stream()
+			.forEach(o -> o.eventoOcorreu(this, evento));
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
 		boolean linhaDiferente = linha != vizinho.linha;
 		boolean colunaDiferente = coluna != vizinho.coluna;
 		boolean diagonal = linhaDiferente && colunaDiferente;
 		
 		int deltaLinha = Math.abs(linha - vizinho.linha);
 		int deltaColuna = Math.abs(coluna - vizinho.coluna);
-		int deltaGeral = deltaColuna + deltaLinha;
+		int detalGeral = deltaColuna + deltaLinha;
 		
-		if(deltaGeral == 1 && !diagonal) {
+		if(detalGeral == 1 && !diagonal) {
 			vizinhos.add(vizinho);
 			return true;
-		}else if(deltaGeral == 2 && diagonal) {
+		} else if(detalGeral == 2 && diagonal) {
 			vizinhos.add(vizinho);
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
@@ -44,32 +52,41 @@ public class Campo {
 	public void alternarMarcacao() {
 		if(!aberto) {
 			marcado = !marcado;
+			
+			if(marcado) {
+				notificarObservadores(CampoEvento.MARCAR);
+			} else {
+				notificarObservadores(CampoEvento.DESMARCAR);			
+			}
 		}
 	}
 	
 	public boolean abrir() {
 		
-		if(!aberto && !marcado) {
-			aberto = true;
-			
+		if(!aberto && !marcado) {			
 			if(minado) {
-				throw new ExplosaoException();
+				notificarObservadores(CampoEvento.EXPLODIR);
+				return true;
 			}
+			
+			setAberto(true);
+			
 			if(vizinhancaSegura()) {
 				vizinhos.forEach(v -> v.abrir());
 			}
+			
 			return true;
 		} else {
-			return false;	
+			return false;			
 		}
 	}
 	
-	boolean vizinhancaSegura(){
+	public boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 	
-	public void minar() {
-			minado = true;
+	void minar() {
+		minado = true;
 	}
 	
 	public boolean isMinado() {
@@ -82,12 +99,16 @@ public class Campo {
 	
 	void setAberto(boolean aberto) {
 		this.aberto = aberto;
+		
+		if(aberto) {
+			notificarObservadores(CampoEvento.ABRIR);
+		}
 	}
 
 	public boolean isAberto() {
 		return aberto;
 	}
-	
+
 	public boolean isFechado() {
 		return !isAberto();
 	}
@@ -106,27 +127,14 @@ public class Campo {
 		return desvendado || protegido;
 	}
 	
-	long minasNaVizinhanca() {
-		return vizinhos.stream().filter(v -> v.minado).count();
+	public int minasNaVizinhanca() {
+		return (int) vizinhos.stream().filter(v -> v.minado).count();
 	}
 	
-	void reiniciar () {
+	void reiniciar() {
 		aberto = false;
 		minado = false;
 		marcado = false;
-	}
-	
-	public String toString() {
-		if(marcado) {
-			return "x";
-		}else if(aberto && minado) {
-			return "*";
-		}else if(aberto && minasNaVizinhanca() > 0) {
-			return Long.toString(minasNaVizinhanca());
-		}else if(aberto) {
-			return " ";
-		}else {
-			return"?";
-		}
+		notificarObservadores(CampoEvento.REINICIAR);
 	}
 }
